a9b42528800977d6656074d29035893118489c57
AllieStewart <megaxis1217@gmail.com>
2023-10-23 17:37:48 -0500

    fixed the... inquirer issue (installed yay), changed some logic in user_input.js, changed conditionals in generateSVG.js; need to fix character length issue in user_input.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // Start of JS file
 // Runs the main application while importing from lib/
 // Would run the user input questions.
-const UI = require('./lib/user_input');
+const UI = require('./lib/user_input.js');
 
 const prog = new UI();
 
