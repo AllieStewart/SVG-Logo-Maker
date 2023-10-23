677fdd0d9d0f8b2f49c1a53615b3684ad8dd53b4
AllieStewart <megaxis1217@gmail.com>
2023-10-23 18:42:31 -0500

    added more comments to all .js files (not test.js ones)

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // Start of JS file
-// Runs the main application while importing from lib/
-// Would run the user input questions.
+// Runs the main application while importing from user_input.js
+// Would run the user input questions, and the general application.
 const UI = require('./lib/user_input.js');
 
 const prog = new UI();
